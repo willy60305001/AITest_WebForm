5da28d272a0c4097d4c9016378709afa7fdb7c31
Willy <willy60305001@gmail.com>
2024-10-18 01:05:11 +0800

    First Commit

---
+++ ASP.NET-Web-Forms-master/07. ASP.NET-File-Upload/Demo/FileUploadInASP.NET/1-SimpleFileUpload.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FileUploadInASP.NET
+{
+    public partial class SimpleFileUpload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void UploadButton_Click(object sender, EventArgs e)
+        {
+            if (FileUploadControl.HasFile)
+            {
+                string filename = Path.GetFileName(FileUploadControl.FileName);
+                FileUploadControl.SaveAs(Server.MapPath("~/Uploaded_Files/") + filename);
+
+                ShowMessage("Upload status: File uploaded!");
+
+            }
+            else
+            {
+                ShowMessage("Upload status: File upload fail !");
+            }
+        }
+
+        public void ShowMessage(string strMsg)
+        {
+            StatusLabel.Text = strMsg;
+        }
+    }
+}
---
+++ ASP.NET-Web-Forms-master/07. ASP.NET-File-Upload/Demo/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using FileUploadInASP.NET;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Web.UI.WebControls;
+using Label = System.Web.UI.WebControls.Label;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void show_message_updates_status_label()
+        {
+            // Arrange
+            var page = new SimpleFileUpload();
+            string expectedMessage = "Test Message";
+
+            // Act
+            page.ShowMessage(expectedMessage);
+
+            // Assert
+            Assert.AreEqual(expectedMessage, "");
+        }
+
+    }
+}
